70d97abccd54fb89adcb4fd8e99a89f41f1d699b
Inna Fryckstedt <innaf@rackham2.uppmax.uu.se>
2022-04-08 16:04:03 +0200

    Quast script with reference genome

--- scripts/Quast_script_assembly_eval.sh
+++ scripts/Quast_script_assembly_eval.sh
@@ -14,5 +14,5 @@ module load bioinfo-tools
 module load quast
 
 # Run quast on genome assembly
-quast.py -o ~/Genome_analysis_project/results/assembly_eval_quast /home/innaf/Genome_analysis_project/results/assembly/genome_assembly_canu.contigs.fasta
+quast.py -o ~/Genome_analysis_project/results/assembly_eval_quast -r /home/innaf/Genome_analysis_project/data/reference_genome/ncbi-genomes-2022-04-08/GCF_009734005.1_ASM973400v2_genomic.fna /home/innaf/Genome_analysis_project/results/assembly/genome_assembly_canu.contigs.fasta
 
